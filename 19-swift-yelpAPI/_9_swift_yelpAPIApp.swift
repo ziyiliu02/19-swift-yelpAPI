c9ac7ef636aaf31b21878393a1afd831b3c5f691
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-20 10:42:14 +0800

    Initial Commit

---
+++ 19-swift-yelpAPI/_9_swift_yelpAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_swift_yelpAPIApp.swift
+//  19-swift-yelpAPI
+//
+//  Created by Liu Ziyi on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct _9_swift_yelpAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
