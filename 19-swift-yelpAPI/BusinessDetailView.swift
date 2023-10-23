844994195db2bd5ea9965ba3a183599ca050ba9b
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-23 18:00:32 +0800

    - style business detail view

--- 19-swift-yelpAPI/BusinessDetailView.swift
+++ 19-swift-yelpAPI/BusinessDetailView.swift
@@ -12,7 +12,77 @@ struct BusinessDetailView: View {
     var business: Business?
     
     var body: some View {
-        Text(business?.name ?? "")
+        VStack(spacing: 0) {
+            // Image
+            ZStack(alignment: .trailing) {
+                Image("detail-placeholder-image")
+                    .resizable()
+                
+                VStack {
+                    Spacer()
+                    Image("yelp-attribution-image")
+                        .frame(width: 72, height: 36)
+                }
+            }
+            .frame(height: 164)
+            
+            // Green Rectangle
+            if let isClosed = business?.isClosed {
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .foregroundColor(isClosed ? .red : .green)
+                    
+                    Text(isClosed ? "Closed" : "Open")
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .frame(height: 36)
+            }
+            
+            // Body
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(business?.name ?? "")
+                        .font(Font.system(size: 21))
+                        .bold()
+                        .padding(.bottom, 10)
+                        .padding(.top, 16)
+                    
+                    Text("\(business?.location?.address1 ?? ""), \(business?.location?.city ?? "")")
+                    Text("\(business?.location?.state ?? "") \(business?.location?.zipCode ?? ""), \(business?.location?.country ?? "")")
+                        .padding(.bottom, 10)
+                    
+                    Image("regular_\(business?.rating ?? 0)")
+                        .padding(.bottom, 16)
+                    
+                    Divider()
+                    
+                    HStack {
+                        Image(systemName: "phone")
+                        Text(business?.phone ?? "")
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                    }
+                    .padding(.vertical, 12)
+                    
+                    Divider()
+                    
+                    HStack {
+                        Image(systemName: "globe")
+                        Text(business?.url ?? "")
+                            .lineLimit(1)
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                    }
+                    .padding(.vertical, 12)
+                    
+                    Divider()
+                    
+                    Spacer()
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
